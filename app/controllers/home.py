39adc184f9a9fd5eae839ce4a2cbe4506b2f2bee
Francis Behnen <francis.behnen@gmail.com>
2019-06-08 00:34:12 +0200

    Add landing page template with email sign up addapted from: https://github.com/BlackrockDigital/startbootstrap-landing-page/

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 blueprint = Blueprint('home', __name__)
 
 @blueprint.route('/')
 def index():
     return render_template('home/index.html')
+
+@blueprint.route('/home')
+def home():
+    email = request.args.get('email', '')
+
+    if email:
+        flash('Thank you for signing up with: ' + email, 'info')
+
+    return render_template('home/home.html')
